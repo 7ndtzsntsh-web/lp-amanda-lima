d30d5d0c5d51c70eb4d73c385415e231735fec1e
7ndtzsntsh-web <7ndtzsntsh@privaterelay.appleid.com>
2026-09-14 23:06:48 -0300

    chore: moved tailwind config to external file

---
+++ js/config.js
@@ -0,0 +1,8 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: { brand: { teal: '#14b8a6', dark: '#0f766e', light: '#ccfbf1' } },
+      fontFamily: { sans: ['Inter', 'sans-serif'] }
+    }
+  }
+}
